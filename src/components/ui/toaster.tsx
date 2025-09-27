089dac810cc81a2ba659e91998e55761d3e9517e
Ukachi Praise <ukachipraise80@gmail.com>
2025-09-27 09:53:38 +0000

    make our error messages cool

--- src/components/ui/toaster.tsx
+++ src/components/ui/toaster.tsx
@@ -9,15 +9,21 @@ import {
   ToastTitle,
   ToastViewport,
 } from "@/components/ui/toast"
+import { TriangleAlert } from "lucide-react";
 
 export function Toaster() {
   const { toasts } = useToast()
 
   return (
     <ToastProvider>
-      {toasts.map(function ({ id, title, description, action, ...props }) {
+      {toasts.map(function ({ id, title, description, action, variant, ...props }) {
         return (
-          <Toast key={id} {...props}>
+          <Toast key={id} variant={variant} {...props}>
+            {variant === 'destructive' && (
+              <div className="pr-4">
+                  <TriangleAlert className="h-6 w-6 text-destructive-foreground" />
+              </div>
+            )}
             <div className="grid gap-1">
               {title && <ToastTitle>{title}</ToastTitle>}
               {description && (
--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -7,6 +7,7 @@ import type {
   ToastActionElement,
   ToastProps,
 } from "@/components/ui/toast"
+import { type VariantProps } from "class-variance-authority"
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
@@ -16,6 +17,7 @@ type ToasterToast = ToastProps & {
   title?: React.ReactNode
   description?: React.ReactNode
   action?: ToastActionElement
+  variant?: VariantProps<any>['variant']
 }
 
 const actionTypes = {
